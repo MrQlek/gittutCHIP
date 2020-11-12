42b3313840c4d256ca3eb229cc12aec2eae984f3
Qlek <MrQlek@gmail.com>
2020-11-12 18:27:49 +0100

    Norweski

--- hello.py
+++ hello.py
@@ -2,6 +2,8 @@ def main():
     name = input("Give name: ")
     if(name == 'Jan'):
         print("Witaj!")
+    elif(name == 'Joaqin'):
+        print("Hei!")
     else:
         print("Hello!")
 
